a75e434f4f68852c78caed71f0ee460cbcc30f17
vitvad <job4vad@gmail.com>
2014-11-30 23:19:04 +0200

    add localization update manifest rewrite background script code style etc.

--- app/js/background.js
+++ app/js/background.js
@@ -1,97 +1,196 @@
+'use strict';
+  /**
+   * @TODO:
+   * - On istall Open page with changelog
+   * - Settings Page
+   * - Custom response
+   */
+
+//chrome.browserAction.setBadgeText({text: '(o_O)|")'});
+
+var debugURLS = ['http://localhost/'];
+
 var accessControlRequestHeaders;
 var exposedHeaders;
 
-var requestListener = function(details){
-	var flag = false,
-		rule = {
-			name: "Origin",
-			value: "http://evil.com/"
-		};
 
-	for (var i = 0; i < details.requestHeaders.length; ++i) {
-		if (details.requestHeaders[i].name === rule.name) {
-			flag = true;
-			details.requestHeaders[i].value = rule.value;
-			break;
-		}
-	}
-	if(!flag) details.requestHeaders.push(rule);
-	
-	for (var i = 0; i < details.requestHeaders.length; ++i) {
-		if (details.requestHeaders[i].name === "Access-Control-Request-Headers") {
-			accessControlRequestHeaders = details.requestHeaders[i].value	
-		}
-	}	
-	
-	return {requestHeaders: details.requestHeaders};
+var requestRules = [{
+  'data': {
+    'name': 'Origin',
+    'value': 'http://evil.com/'
+  },
+  'mandatory': true,
+  'fn': null
+}, {
+  'data': {
+    'name': 'Access-Control-Request-Headers',
+    'value': null,
+  },
+  'mandatory': false,
+  'fn': function(rule, header) {
+    accessControlRequestHeaders = header.value;
+  }
+}];
+
+
+var responseRules = [{
+  'data': {
+    'name': 'Access-Control-Allow-Origin',
+    'value': '*'
+  },
+  'mandatory': true,
+  'fn': null
+}, {
+  'data': {
+    'name': 'Access-Control-Allow-Headers',
+    'value': accessControlRequestHeaders
+  },
+  'mandatory': true,
+  'fn': null
+}, {
+  'data': {
+    'name': 'Access-Control-Allow-Credentials',
+    'value': 'true'
+  },
+  'mandatory': false,
+  'fn': null
+}, {
+  'data': {
+    'name': 'Access-Control-Allow-Methods',
+    'value': 'POST, GET, OPTIONS, PUT'
+  },
+  'mandatory': true,
+  'fn': null
+}];
+
+var requestListener = function(details) {
+  requestRules.forEach(function(rule) {
+    var flag = false;
+
+    details.requestHeaders.forEach(function(header) {
+      if (header.name === rule.data.name) {
+        flag = true;
+        if (rule.fn) {
+          rule.fn.call(null, rule, header);
+        } else {
+          header.value = rule.data.value;
+        }
+      }
+    });
+
+    //add this rule anyway if it's not present in request headers
+    if (!flag && rule.mandatory && rule.data.value) {
+      details.requestHeaders.push(rule.data);
+    }
+  });
+
+  if (debugURLS.indexOf(details.url) > -1) {
+    console.group('Request');
+    console.log(JSON.stringify(details, null, 2));
+    console.groupEnd('Request');
+  }
+
+  return {
+    requestHeaders: details.requestHeaders
+  };
 };
 
-var responseListener = function(details){
-	var flag = false,
-	rule = {
-			"name": "Access-Control-Allow-Origin",
-			"value": "*"
-		};
+var responseListener = function(details) {
+  var headers = responseRules.filter(function(rule) {
+    var und; //undefined
+    return rule.value !== und && rule.value !== null;
+  });
 
-	for (var i = 0; i < details.responseHeaders.length; ++i) {
-		if (details.responseHeaders[i].name === rule.name) {
-			flag = true;
-			details.responseHeaders[i].value = rule.value;
-			break;
-		}
-	}
-	if(!flag) details.responseHeaders.push(rule);
+  responseRules.forEach(function(rule) {
+    var flag = false;
 
-	if (accessControlRequestHeaders) {
+    details.responseHeaders.forEach(function(header) {
+      // if rule exist in response - rewrite value
+      if (header.name === rule.data.name) {
+        flag = true;
+        if (rule.fn) {
+          rule.fn.call(null, rule.data, header);
+        } else {
+          if (rule.data.value) {
+            header.value = rule.data.value;
+          } else {
+            //@TODO DELETE this header
+          }
+        }
+      }
+    });
 
-		details.responseHeaders.push({"name": "Access-Control-Allow-Headers", "value": accessControlRequestHeaders});
+    //add this rule anyway if it's not present in request headers
+    if (!flag && rule.mandatory && rule.data.value) {
+      details.responseHeaders.push(rule.data);
+    }
+  });
 
-	}
+  //details.responseHeaders = details.responseHeaders.concat(headers);
 
-	if(exposedHeaders) {
-		details.responseHeaders.push({"name": "Access-Control-Expose-Headers", "value": exposedHeaders});
-	}
 
-	details.responseHeaders.push({"name": "Access-Control-Allow-Methods", "value": "GET, PUT, POST, DELETE, HEAD"});
+  //@todo REMOVE test
+  if (debugURLS.indexOf(details.url) > -1) {
+    console.group('Response');
+    console.log(JSON.stringify(headers, null, 2));
+    console.log(JSON.stringify(details, null, 2));
+    console.groupEnd('Response');
+  }
+  return {
+    responseHeaders: details.responseHeaders
+  };
+};
 
-	return {responseHeaders: details.responseHeaders};
-	
+/*Reload settings*/
+var reload = function () {
+  chrome.storage.local.get({
+      'active': false,
+      'urls': ['*://*/*'],
+      'exposedHeaders': ''
+    },
+    function(result) {
+      exposedHeaders = result.exposedHeaders;
+      console.info(exposedHeaders);
+
+      /*Remove Listeners*/
+      chrome.webRequest.onHeadersReceived.removeListener(responseListener);
+      chrome.webRequest.onBeforeSendHeaders.removeListener(requestListener);
+
+      if (result.active) {
+        chrome.browserAction.setIcon({
+          path: 'images/on.png'
+        });
+
+        if (result.urls.length) {
+          /*Add Listeners*/
+          chrome.webRequest.onHeadersReceived.addListener(responseListener, {
+            urls: result.urls
+          }, ['blocking', 'responseHeaders']);
+
+          chrome.webRequest.onBeforeSendHeaders.addListener(requestListener, {
+            urls: result.urls
+          }, ['blocking', 'requestHeaders']);
+        }
+      } else {
+        chrome.browserAction.setIcon({
+          path: 'images/off.png'
+        });
+      }
+    });
 };
 
 /*On install*/
-chrome.runtime.onInstalled.addListener(function(){
-	chrome.storage.local.set({'active': false});
-	chrome.storage.local.set({'urls': ["*://*/*"]});
-	chrome.storage.local.set({'exposedHeaders': ''});
-	reload();
+chrome.runtime.onInstalled.addListener(function(details) {
+  console.log('previousVersion', JSON.stringify(details, null, 2));
+
+  chrome.storage.local.set({
+    'active': true
+  });
+  chrome.storage.local.set({
+    'urls': ['*://*/*']
+  });
+  chrome.storage.local.set({
+    'exposedHeaders': ''
+  });
+  reload();
 });
-
-/*Reload settings*/
-function reload() {
-	chrome.storage.local.get({'active': false, 'urls': ["*://*/*"], 'exposedHeaders': ''}, function(result) {
-
-		exposedHeaders = result.exposedHeaders;
-
-		/*Remove Listeners*/
-		chrome.webRequest.onHeadersReceived.removeListener(responseListener);
-		chrome.webRequest.onBeforeSendHeaders.removeListener(requestListener);
-
-		if(result.active) {
-			chrome.browserAction.setIcon({path: "on.png"});
-
-			if(result.urls.length) {
-
-				/*Add Listeners*/
-				chrome.webRequest.onHeadersReceived.addListener(responseListener, {
-					urls: result.urls
-				},["blocking", "responseHeaders"]);
-
-				chrome.webRequest.onBeforeSendHeaders.addListener(requestListener, {
-					urls: result.urls
-				},["blocking", "requestHeaders"]);
-			}
-		} else {
-			chrome.browserAction.setIcon({path: "off.png"});
-		}
-	});
-}
--- app/js/popup.js
+++ app/js/popup.js
@@ -1,87 +1,107 @@
+'use strict';
+/* global angular */
 var app = angular.module('cors', ['ionic']);
 
 app.controller('PopupCtrl', ['$scope', function($scope) {
 
-	$scope.active = false;
-	$scope.urls = [];
-	$scope.url = '';
-	$scope.exposedHeaders = '';
+  $scope.active = false;
+  $scope.urls = [];
+  $scope.url = '';
+  $scope.exposedHeaders = '';
+  $scope.i18n = {
+    settings: chrome.i18n.getMessage('settings'),
+    allowHeaders: chrome.i18n.getMessage('allowHeaders'),
+    urlPatterns: chrome.i18n.getMessage('urlPatterns'),
+    sharingHeader: chrome.i18n.getMessage('sharingHeader')
+  };
 
-	chrome.storage.local.get({'active': false, 'urls': [], 'exposedHeaders': ''}, function(result) {
-		$scope.active = result.active;
-		$scope.urls = result.urls;
-		$scope.exposedHeaders = result.exposedHeaders;
-		$scope.$apply();
+  chrome.storage.local.get({
+    'active': $scope.active,
+    'urls': [],
+    'exposedHeaders': ''
+  }, function(result) {
+    $scope.active = result.active;
+    $scope.urls = result.urls;
+    $scope.exposedHeaders = result.exposedHeaders;
+    $scope.$apply();
 
-		$scope.$watch('active', function(newValue, oldValue) {
-			chrome.storage.local.set({'active': $scope.active});
-			chrome.extension.getBackgroundPage().reload();
-		});
+    $scope.$watch('active', function() {
+      chrome.storage.local.set({
+        'active': $scope.active
+      });
+      chrome.extension.getBackgroundPage().reload();
+    });
 
-		$scope.$watch('exposedHeaders', function(newValue, oldValue) {
-			chrome.storage.local.set({'exposedHeaders': $scope.exposedHeaders});
-			chrome.extension.getBackgroundPage().reload();
-		});
+    $scope.$watch('exposedHeaders', function() {
+      chrome.storage.local.set({
+        'exposedHeaders': $scope.exposedHeaders
+      });
+      chrome.extension.getBackgroundPage().reload();
+    });
 
-		$scope.$watch('urls', function(newValue, oldValue) {
-			chrome.storage.local.set({'urls': $scope.urls});
-			chrome.extension.getBackgroundPage().reload();
-		});
-	});
+    $scope.$watch('urls', function() {
+      chrome.storage.local.set({
+        'urls': $scope.urls
+      });
+      chrome.extension.getBackgroundPage().reload();
+    });
+  });
 
-	$scope.openInNewTab = function(url) {
-		chrome.tabs.create({ url: url });
-	}
+  $scope.openInNewTab = function(url) {
+    chrome.tabs.create({
+      url: url
+    });
+  };
 
-	$scope.addUrl = function() {
-		if($scope.url && $.inArray($scope.url, $scope.urls) == -1) {
-			$scope.urls.unshift($scope.url);
-		}
-		$scope.url = '';
-	};
+  $scope.addUrl = function() {
+    if ($scope.url && angular.inArray($scope.url, $scope.urls) === -1) {
+      $scope.urls.unshift($scope.url);
+    }
+    $scope.url = '';
+  };
 
-	$scope.removeUrl = function(index) {
-		$scope.urls.splice(index, 1);
-	};
+  $scope.removeUrl = function(index) {
+    $scope.urls.splice(index, 1);
+  };
 }]);
 
-app.directive("textOption", function() {
-	return {
-		restrict: 'E',
-		scope: {
-			option: '=',
-			placeholder: '@'
-		},
-		templateUrl: 'text-option.html',
-		controller : function($scope) {
-			$scope.editing = false;
+app.directive('textOption', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      option: '=',
+      placeholder: '@'
+    },
+    templateUrl: '../components/text-option.html',
+    controller: function($scope) {
+      $scope.editing = false;
 
-			$scope.onEdit = function() {
-				$scope.editableOption = $scope.option;
-				$scope.editing = true;
-			};
+      $scope.onEdit = function() {
+        $scope.editableOption = $scope.option;
+        $scope.editing = true;
+      };
 
-			$scope.onCancel = function() {
-				$scope.editing = false;
-			};
+      $scope.onCancel = function() {
+        $scope.editing = false;
+      };
 
-			$scope.onSave = function() {
-				$scope.option = $scope.editableOption;
-				$scope.editing = false;
-			};
-		}
-	};
+      $scope.onSave = function() {
+        $scope.option = $scope.editableOption;
+        $scope.editing = false;
+      };
+    }
+  };
 });
 
 app.directive('submitOnEnter', function() {
-	return {
-		restrict: 'A',
-		link: function(scope, element, attrs) {
-			$(element).on('keydown', function(e) {
-				if (e.which == 13) {
-					$(element).parents('.item').find('.submit-action').trigger('click');
-				}
-			});
-		}
-	};
+  return {
+    restrict: 'A',
+    link: function(scope, element) {
+      angular.element(element).on('keydown', function(e) {
+        if (e.keyCode === 13) {
+          angular.element(element).parent().parent().find('.submit-action').click();
+        }
+      });
+    }
+  };
 });
